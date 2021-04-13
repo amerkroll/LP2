ff2acba911f4224a4e43929e5f452fdb7f657cbd
Andrea Mercado <amerkroll@gmail.com>
2021-04-13 15:56:18 -0400

    20210413 - Agregamos clase Lista, aprendimos foreach, agregamos nodos a la lista, imprimimos

---
+++ ClasesLP2-2021/ProyectodeAula2021/Clases/Lista.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProyectodeAula2021.Clases
+{
+    public class Lista
+    {
+
+        static void ImprimirLista()
+        {
+            string profe = "Andrea";
+            List<string> listaAlumnos = new List<string>();
+
+            listaAlumnos.Add("Augusto");
+            listaAlumnos.Add("Meli");
+            listaAlumnos.Add("Edgar");
+            listaAlumnos.Add(profe);
+
+            Console.WriteLine(listaAlumnos[1]);
+            int contador = 0; 
+            foreach (string alumno in listaAlumnos)   // para cada elemento
+            //for (indiceInicio; incremento; condicionStop (cantidad de elementos))
+            //for (int i = 0; i < cantidad; i++ )
+            {
+                //Console.WriteLine(alumno);
+                Console.WriteLine("Elemento #{"+contador+"}--> "+alumno);
+                contador++;
+            }
+
+        }
+    }
+}
--- ClasesLP2-2021/ProyectodeAula2021/Program.cs
+++ ClasesLP2-2021/ProyectodeAula2021/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ProyectodeAula2021.Clases;
 
 namespace ProyectodeAula2021
 {
@@ -16,16 +17,43 @@ namespace ProyectodeAula2021
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            //Application.Run(new Login());
 
             Console.WriteLine("Hola Mundo");
 
             // Call the constructor that has one parameter.
-            var person2 = new Persona();
+            //var person2 = new Persona();
 
-            Console.WriteLine(person2.Nombre);   
-            Console.WriteLine(person2.Apellido);
-            Console.WriteLine(person2);
+            //Console.WriteLine(person2.Nombre);   
+            //Console.WriteLine(person2.Apellido);
+            //Console.WriteLine(person2);
+            ImprimirLista();
+           
+        }
+
+
+        static void ImprimirLista()
+        {
+            string profe = "Andrea";
+            List<string> listaAlumnos = new List<string>();
+
+            listaAlumnos.Add("Augusto");
+            listaAlumnos.Add("Meli");
+            listaAlumnos.Add("Edgar");
+            listaAlumnos.Add(profe);
+
+            Console.WriteLine(listaAlumnos[1]);
+
+            int contador = 0;
+            foreach (string alumno in listaAlumnos)   // para cada elemento
+            //for (indiceInicio; incremento; condicionStop (cantidad de elementos))
+            //for (int i = 0; i < cantidad; i++ )
+            {
+                //Console.WriteLine(alumno);
+                Console.WriteLine("Elemento #{" + contador.ToString() + "}--> " + alumno);
+                contador++;
+            }
+
         }
     }
 }
