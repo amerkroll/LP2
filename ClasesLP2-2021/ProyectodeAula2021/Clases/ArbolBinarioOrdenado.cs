96fdb870ef0d3cba338a3e864153ad8b94215dd2
Andrea Mercado <amerkroll@gmail.com>
2021-05-11 08:46:34 -0400

    20210511 - Agregamos comentarios a ABB.cs

--- ClasesLP2-2021/ProyectodeAula2021/Clases/ArbolBinarioOrdenado.cs
+++ ClasesLP2-2021/ProyectodeAula2021/Clases/ArbolBinarioOrdenado.cs
@@ -19,7 +19,8 @@ namespace ProyectodeAula2021.Clases
         {
             raiz=null;
         }
-      
+        /*Creamos un nodo y disponemos los punteros izq y der a null, guardamos la información que llega al método en el nodo.*/
+        
         public void Insertar (int info)
         {
             Nodo nuevo;
@@ -27,12 +28,15 @@ namespace ProyectodeAula2021.Clases
             nuevo.info = info;
             nuevo.izq = null;
             nuevo.der = null;
-            if (raiz == null)
+            if (raiz == null) //Si el árbol está vacío, apuntamos raíz al nodo creado;
                 raiz = nuevo;
-            else
+            else //en caso de no estar vacío, dentro de una estructura repetitiva vamos comparando info con la información del nodo, 
             {
                 Nodo anterior = null, reco;
                 reco = raiz;
+                //si info es mayor a la del nodo descendemos por el subárbol derecho en caso contrario descendemos por el subárbol izquierdo.
+                //Cuando se encuentra un subárbol vacío insertar el nodo en dicho subárbol. 
+                //Para esto llevamos un puntero anterior dentro del while.
                 while (reco != null)
                 {
                     anterior = reco;
@@ -48,7 +52,15 @@ namespace ProyectodeAula2021.Clases
             }
         }
 
+        /*
+        El método ImprimirPre(), es decir el no recursivo se encarga de llamar al método recursivo pasando la dirección del nodo raiz.
+        El método recursivo void ImprimirPre (Nodo reco) lo primero que verifica con un if si reco está apuntando a un nodo (esto es verdad si reco es distinto a null), en caso afirmativo ingresa al bloque del if y realiza:
 
+     - Visitar la raiz.
+     - Recorrer el subárbol izquierdo en pre-orden.
+     - Recorrer el subárbol derecho en pre-orden.
+        La visita en este caso es la impresión de la información del nodo y los recorridos son las llamadas recursivas pasando las direcciones de los subárboles izquierdo y derecho.
+         */
         private void ImprimirPre (Nodo reco)
         {
             if (reco != null)
@@ -65,6 +77,7 @@ namespace ProyectodeAula2021.Clases
             Console.WriteLine();
         }
 
+        //Los algoritmos de los recorridos en entreorden y postorden son similares. La diferencia es que la visita la realizamos entre las llamadas recursivas en el recorrido en entre orden:
         private void ImprimirEntre (Nodo reco)
         {
             if (reco != null)
@@ -81,7 +94,7 @@ namespace ProyectodeAula2021.Clases
             Console.WriteLine();
         }
 
-
+        //y por último en el recorrido en postorden la visita la realizamos luego de las dos llamadas recursivas:
         private void ImprimirPost (Nodo reco)
         {
             if (reco != null)
