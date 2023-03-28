b38f53793cb9e9caa2260beaa5f9adf9e364ee76
leguizlaila <leguizamonlaila6@gmail.com>
2023-03-28 15:33:02 -0400

    Cambio de nombre level1 en el Form Login

--- ClasesLP2-2021/ProyectodeAula2021/Login.Designer.cs
+++ ClasesLP2-2021/ProyectodeAula2021/Login.Designer.cs
@@ -41,9 +41,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(82, 90);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(44, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Nombre";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
